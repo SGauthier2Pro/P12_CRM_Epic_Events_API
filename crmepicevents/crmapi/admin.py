f4bdcf8abc98976377ad5a5d5f8c95b1c930c720
SGauthier2Pro <Sylvain.Gauthier2Pro@gmail.com>
2023-05-26 09:44:52 +0200

    configuring filters for admin interface

--- crmepicevents/crmapi/admin.py
+++ crmepicevents/crmapi/admin.py
@@ -4,6 +4,17 @@ from crmapi.models.client import Client
 from crmapi.models.contract import Contract
 from crmapi.models.event import Event
 
-admin.site.register(Client)
-admin.site.register(Contract)
-admin.site.register(Event)
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_filter = ["confirmed", "sales_contact"]
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_filter = ["status"]
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_filter = ["support_contact"]
